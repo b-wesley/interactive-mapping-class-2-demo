a39bd97606d894ef746a279e0ae945f804c5cd92
b-wesley <brantw48@gmail.com>
2026-03-26 19:54:46 -0400

    started doing the demo

---
+++ scripts.js
@@ -0,0 +1,21 @@
+const creatures_seen = 0;
+
+function show_me_squids() {
+  document.body.style.backgroundImage = "url('https://static.tvtropes.org/pmwiki/pub/images/the_two_faces_of_squidward.png')";
+}
+
+function gimme_sharks() {
+  document.body.style.backgroundImage = "url('https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/White_shark.jpg/330px-White_shark.jpg')";
+}
+
+function go_to_the_reef() {
+  document.body.style.backgroundImage = "url('https://www.gdrc.org/oceans/artificial-reefs.jpg')";
+}
+
+function meet_the_blob() {
+  document.body.style.backgroundImage = "url('https://cafishvet.com/wp-content/uploads/2013/05/weirdest-sea-10-blobfish.jpg')";
+}
+
+function cat_fish() {
+  document.body.style.backgroundImage = "url('https://bloximages.newyork1.vip.townnews.com/oudaily.com/content/tncms/assets/v3/editorial/8/53/85358c06-9ed1-11e6-b01e-97f260a3b131/581640cfb2288.image.jpg')";
+}
